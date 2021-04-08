ab4af7c87c0d5dd0834b6775ca795c42e9d7382f
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-08 11:53:09 +0200

    Add components

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright 2021 Zyar3k</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,25 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A quam repellat
+        totam vero fugit. Quae porro neque, qui rerum, earum possimus incidunt
+        nemo debitis ut minus, distinctio amet officiis deserunt. Voluptatem
+        architecto eos, quisquam, nesciunt doloribus nisi, aspernatur sit
+        sapiente delectus neque quam adipisci doloremque tenetur consequuntur
+        labore ipsa omnis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A quam repellat
+        totam vero fugit. Quae porro neque, qui rerum, earum possimus incidunt
+        nemo debitis ut minus, distinctio amet officiis deserunt. Voluptatem
+        architecto eos, quisquam, nesciunt doloribus nisi, aspernatur sit
+        sapiente delectus neque quam adipisci doloremque tenetur consequuntur
+        labore ipsa omnis.
+      </p>
+    </div>
+  );
+};
+
+export default About;
