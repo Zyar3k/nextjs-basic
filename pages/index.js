fc404f744079b95d9f01357230602efa2f72cda2
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-08 12:04:11 +0200

    Add layout component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 import Link from "next/link";
@@ -8,7 +6,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>home</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. A quam repellat
@@ -21,7 +18,6 @@ export default function Home() {
       <Link href='/people'>
         <a>See People Listing</a>
       </Link>
-      <Footer />
     </div>
   );
 }
