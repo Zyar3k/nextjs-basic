e9cae91fbf850eb0e76196676f062cc7ae6319c4
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-08 13:13:12 +0200

    Fetch data

--- pages/people/index.js
+++ pages/people/index.js
@@ -1,7 +1,16 @@
 import Head from "next/head";
 import styles from "../../styles/People.module.css";
 
-const People = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { people: data },
+  };
+};
+
+const People = ({ people }) => {
   return (
     <>
       <Head>
@@ -10,6 +19,13 @@ const People = () => {
       </Head>
       <div>
         <h1>People</h1>
+        {people.map((person) => (
+          <div key={person.id}>
+            <a className={styles.single}>
+              <h3>{person.name}</h3>
+            </a>
+          </div>
+        ))}
       </div>
     </>
   );
