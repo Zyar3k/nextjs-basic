62827004c260189cba6d2e082738b96414fd691c
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-08 15:22:07 +0200

    Add fetch to single item

--- pages/people/[id].js
+++ pages/people/[id].js
@@ -1,4 +1,4 @@
-export const getStaticPath = async () => {
+export const getStaticPaths = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
 
@@ -14,10 +14,23 @@ export const getStaticPath = async () => {
   };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
+
+  return {
+    props: { person: data },
+  };
+};
+
+const Details = ({ person }) => {
   return (
     <div>
-      <h1>Details page</h1>
+      <h1>{person.name}</h1>
+      <p>{person.email}</p>
+      <p>{person.website}</p>
+      <p>{person.address.city}</p>
     </div>
   );
 };
